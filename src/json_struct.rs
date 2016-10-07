705b969bccc4ef2ce4959dbe0a2d732ac6dcc0cf
Silvio Fricke <silvio.fricke@gmail.com>
2016-10-07 22:13:02 +0200

    switch to serde_json
    Signed-off-by: Silvio Fricke <silvio.fricke@gmail.com>

--- src/json_struct.rs
+++ src/json_struct.rs
@@ -3,29 +3,30 @@ use std::iter::Enumerate;
 use std::collections::btree_map::Iter as MapIter;
 use std::slice::Iter as VecIter;
 
-use rustc_serialize::json::Json;
+use serde_json::value::Value;
 
 
 enum StackItem<'a> {
-    MapIter(MapIter<'a, String, Json>),
-    VecIter(Enumerate<VecIter<'a, Json>>),
+    MapIter(MapIter<'a, String, Value>),
+    VecIter(Enumerate<VecIter<'a, Value>>),
 }
 
 
-/// Converts JSON structure from `rustc_serialize::json::Json` into gron format
+/// Converts JSON string into gron format
 ///
 /// # Example
 ///
 /// ```
 /// # extern crate gron;
-/// # extern crate rustc_serialize;
+/// # extern crate serde_json;
 /// #
 /// # use std::io::stdout;
-/// # use rustc_serialize::json::Json;
+/// # use serde_json::value::Value;
+/// # use serde_json::de;
 /// # use gron::json_to_gron;
 /// #
 /// # fn main() {
-/// let json = Json::from_str(r#"{"x": [1,2]}"#).unwrap();
+/// let json = de::from_str(r#"{"x": [1,2]}"#).unwrap();
 /// json_to_gron(&mut stdout(), "val", &json);
 /// // Outputs to stdout:
 /// //
@@ -36,7 +37,7 @@ enum StackItem<'a> {
 /// # }
 ///
 /// ```
-pub fn json_to_gron<W: Write>(out: &mut W, prefix: &str, json: &Json)
+pub fn json_to_gron<W: Write>(out: &mut W, prefix: &str, json: &Value)
     -> io::Result<()>
 {
     use self::StackItem::*;
@@ -45,22 +46,22 @@ pub fn json_to_gron<W: Write>(out: &mut W, prefix: &str, json: &Json)
     let mut namebuf = String::with_capacity(100);
     namebuf.push_str(prefix);
     match *json {
-        Json::I64(value) => try!(writeln!(out, "{} = {}", namebuf, value)),
-        Json::U64(value) => try!(writeln!(out, "{} = {}", namebuf, value)),
-        Json::F64(value) => try!(writeln!(out, "{} = {}", namebuf, value)),
-        Json::String(ref value) => {
+        Value::I64(value) => try!(writeln!(out, "{} = {}", namebuf, value)),
+        Value::U64(value) => try!(writeln!(out, "{} = {}", namebuf, value)),
+        Value::F64(value) => try!(writeln!(out, "{} = {}", namebuf, value)),
+        Value::String(ref value) => {
             try!(writeln!(out, "{} = {:?}", namebuf, value));
         }
-        Json::Boolean(value) => try!(writeln!(out, "{} = {}", namebuf, value)),
-        Json::Array(ref vec) => {
+        Value::Bool(value) => try!(writeln!(out, "{} = {}", namebuf, value)),
+        Value::Array(ref vec) => {
             try!(writeln!(out, "{} = []", namebuf));
             stack.push((VecIter(vec.iter().enumerate()), namebuf.len()));
         }
-        Json::Object(ref keys) => {
+        Value::Object(ref keys) => {
             try!(writeln!(out, "{} = {{}}", namebuf));
             stack.push((MapIter(keys.iter()), namebuf.len()));
         }
-        Json::Null => try!(writeln!(out, "{} = null", namebuf)),
+        Value::Null => try!(writeln!(out, "{} = null", namebuf)),
     }
     while stack.len() > 0 {
         let (kind, off) = stack.pop().unwrap();
@@ -73,31 +74,31 @@ pub fn json_to_gron<W: Write>(out: &mut W, prefix: &str, json: &Json)
                 };
                 stack.push((MapIter(iter), off));
                 match *json {
-                    Json::I64(value) => {
+                    Value::I64(value) => {
                         try!(writeln!(out, "{}.{} = {}",
                             namebuf, key, value));
                     }
-                    Json::U64(value) => {
+                    Value::U64(value) => {
                         try!(writeln!(out, "{}.{} = {}",
                             namebuf, key, value));
                     }
-                    Json::F64(value) => {
+                    Value::F64(value) => {
                         try!(writeln!(out, "{}.{} = {}",
                             namebuf, key, value));
                     }
-                    Json::String(ref value) => {
+                    Value::String(ref value) => {
                         try!(writeln!(out, "{}.{} = {:?}",
                             namebuf, key, value));
                     }
-                    Json::Boolean(value) => {
+                    Value::Bool(value) => {
                         try!(writeln!(out, "{}.{} = {}",
                             namebuf, key, value));
                     }
-                    Json::Null => {
+                    Value::Null => {
                         try!(writeln!(out, "{}.{} = null",
                             namebuf, key));
                     }
-                    Json::Array(ref vec) => {
+                    Value::Array(ref vec) => {
                         namebuf.push('.');
                         namebuf.push_str(key);
                         try!(writeln!(out, "{} = []", namebuf));
@@ -105,7 +106,7 @@ pub fn json_to_gron<W: Write>(out: &mut W, prefix: &str, json: &Json)
                             VecIter(vec.iter().enumerate()),
                             namebuf.len()));
                     }
-                    Json::Object(ref keys) => {
+                    Value::Object(ref keys) => {
                         namebuf.push('.');
                         namebuf.push_str(key);
                         try!(writeln!(out, "{} = {{}}", namebuf));
@@ -120,31 +121,31 @@ pub fn json_to_gron<W: Write>(out: &mut W, prefix: &str, json: &Json)
                 };
                 stack.push((VecIter(iter), off));
                 match *json {
-                    Json::I64(value) => {
+                    Value::I64(value) => {
                         try!(writeln!(out, "{}[{}] = {}",
                             namebuf, index, value));
                     }
-                    Json::U64(value) => {
+                    Value::U64(value) => {
                         try!(writeln!(out, "{}[{}] = {}",
                             namebuf, index, value));
                     }
-                    Json::F64(value) => {
+                    Value::F64(value) => {
                         try!(writeln!(out, "{}[{}] = {}",
                             namebuf, index, value));
                     }
-                    Json::String(ref value) => {
+                    Value::String(ref value) => {
                         try!(writeln!(out, "{}[{}] = {:?}",
                             namebuf, index, value));
                     }
-                    Json::Boolean(value) => {
+                    Value::Bool(value) => {
                         try!(writeln!(out, "{}[{}] = {}",
                             namebuf, index, value));
                     }
-                    Json::Null => {
+                    Value::Null => {
                         try!(writeln!(out, "{}[{}] = null",
                             namebuf, index));
                     }
-                    Json::Array(ref vec) => {
+                    Value::Array(ref vec) => {
                         use std::fmt::Write;
                         write!(&mut namebuf, "[{}]", index).unwrap();
                         try!(writeln!(out, "{} = []", namebuf));
@@ -152,7 +153,7 @@ pub fn json_to_gron<W: Write>(out: &mut W, prefix: &str, json: &Json)
                             VecIter(vec.iter().enumerate()),
                             namebuf.len()));
                     }
-                    Json::Object(ref keys) => {
+                    Value::Object(ref keys) => {
                         use std::fmt::Write;
                         write!(&mut namebuf, "[{}]", index).unwrap();
                         try!(writeln!(out, "{} = {{}}", namebuf));
@@ -168,14 +169,15 @@ pub fn json_to_gron<W: Write>(out: &mut W, prefix: &str, json: &Json)
 
 #[cfg(test)]
 mod test {
-    use rustc_serialize::json::Json;
+    use serde_json::value::Value;
+    use serde_json::de;
 
     use super::json_to_gron;
 
     fn assert_equal(json_src: &str, gron: &str) {
         let mut buf = Vec::new();
         json_to_gron(&mut buf, "json",
-            &Json::from_str(json_src).unwrap()).unwrap();
+            &de::from_str(json_src).unwrap()).unwrap();
         assert_eq!(String::from_utf8(buf).unwrap(), gron);
     }
 
